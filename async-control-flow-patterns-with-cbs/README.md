fe6cecf310f21c20e07d70a136bf5107d1967cfa
Or Hasson <hassonor@gmail.com>
2022-06-11 11:13:48 +0300

    web spider with controlling concurrency with generic task queue

--- async-control-flow-patterns-with-cbs/README.md
+++ async-control-flow-patterns-with-cbs/README.md
@@ -8,3 +8,70 @@ ___
   current statement instead of writing (and nesting) complete if...else statements.
 * Create named functions for callbacks, keeping them out of closures and passing intermediate results as arguments.
 * Modularize the code. Split the code into smalled, reusable functions whenever possible.
+
+#### Pattern sequential execution
+
+```node
+    function iterate(index) {
+    if (index === tasks.length) {
+        return finish();
+    }
+    const task = tasks[index];
+    task(() => iterate(index + 1);
+}
+
+function finish() {
+    // iteration complete
+}
+
+iterate(0);
+```
+
+#### Pattern parallel execution:
+
+```node
+const tasks = [/* ... */];
+
+let completed = 0;
+tasks.forEach(task => {
+    task(() => {
+        if (++completed === tasks.length) {
+            finish()
+        }
+    })
+})
+```
+
+#### Pattern Limiting concurrency:
+
+___
+This algorithm can be considered a mixture of sequential execution and parallel execution.
+
+```node
+const tasks = [
+    //...
+]
+const concurrency = 2;
+let running = 0;
+let completed = 0;
+let index = 0;
+
+function next() {
+    while (running < concurrency && index < tasks.length) {
+        task(() => {
+            if (++completed === tasks.length) {
+                return finish();
+            }
+            running--;
+            next();
+        })
+        running++;
+    }
+}
+
+next();
+
+function finish() {
+    // all tasks finished
+}
+```
--- async-control-flow-patterns-with-cbs/web-spider-with-task-queue/spider.js
+++ async-control-flow-patterns-with-cbs/web-spider-with-task-queue/spider.js
@@ -29,58 +29,20 @@ function download(url, filename, cb) {
     })
 }
 
-function spiderLinks(currentUrl, body, nesting, cb) {
+function spiderLinks(currentUrl, body, nesting, queue) {
     if (nesting === 0) {
-        return process.nextTick(cb)
+        return
     }
 
     const links = getPageLinks(currentUrl, body)
     if (links.length === 0) {
-        return process.nextTick(cb)
+        return
     }
 
-    let completed = 0;
-    let hasErrors = false;
-
-    function done(err) {
-        if (err) {
-            hasErrors = true;
-            return cb(err);
-        }
-        if (++completed === links.length && !hasErrors) {
-            return cb();
-        }
-    }
-
-    links.forEach(link => spider(link, nesting - 1, done));
-
+    links.forEach(link => spider(link, nesting - 1, queue))
 }
 
-/*
-    The Pattern:
-
-    function iterate(index){
-        if(index === tasks.length) {
-            return finish();
-        }
-        const task = tasks[index];
-        task(()=> iterate(index + 1);
-    }
-
-    function finish(){
-        // iteration complete
-    }
-
-    iterate(0);
- */
-const spidering = new Set();
-
-export function spider(url, nesting, cb) {
-    if (spidering.has(url)) {
-        return process.nextTick(cb);
-    }
-    spidering.add(url);
-    
+function spiderTask(url, nesting, queue, cb) {
     const filename = urlToFilename(url)
     fs.readFile(filename, 'utf8', (err, fileContent) => {
         if (err) {
@@ -88,17 +50,30 @@ export function spider(url, nesting, cb) {
                 return cb(err)
             }
 
-            // The file doesn't exist, so let’s download it
             return download(url, filename, (err, requestContent) => {
                 if (err) {
                     return cb(err)
                 }
 
-                spiderLinks(url, requestContent, nesting, cb)
+                spiderLinks(url, requestContent, nesting, queue)
+                return cb()
             })
         }
 
-        // The file already exists, let’s process the links
-        spiderLinks(url, fileContent, nesting, cb)
+        spiderLinks(url, fileContent, nesting, queue)
+        return cb()
+    })
+}
+
+const spidering = new Set()
+
+export function spider(url, nesting, queue) {
+    if (spidering.has(url)) {
+        return
+    }
+
+    spidering.add(url)
+    queue.pushTask((done) => {
+        spiderTask(url, nesting, queue, done)
     })
 }
